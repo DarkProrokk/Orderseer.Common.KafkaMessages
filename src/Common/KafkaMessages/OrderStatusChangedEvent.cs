a961ccc5f92341fdfc59de78ad53213c1327a452
DarkProrok <seroshtanmakcs@gmail.com>
2025-03-09 01:50:53 +1000

    add detail to OrderStatusChangedEvent.cs

--- src/Common/KafkaMessages/OrderStatusChangedEvent.cs
+++ src/Common/KafkaMessages/OrderStatusChangedEvent.cs
@@ -2,5 +2,13 @@ namespace KafkaMessages;
 
 public class OrderStatusChangedEvent
 {
+    public Guid OrderId { get; set; }
     
+    public required Status Status { get; set; }
+}
+
+public class Status
+{
+    public int code { get; set; } 
+    public string name { get; set; }
 }
